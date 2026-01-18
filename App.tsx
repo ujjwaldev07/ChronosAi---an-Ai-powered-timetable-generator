9979818807df1252b297eceaa36995f1c39d7b50
Ujjwal Singh <vilenofficial664@gmail.com>
2026-01-18 20:14:50 +0530

    Remove light mode class toggle in dark mode

--- App.tsx
+++ App.tsx
@@ -20,9 +20,7 @@ const App: React.FC = () => {
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add('dark');
-      document.documentElement.classList.remove('light');
     } else {
-      document.documentElement.classList.add('light');
       document.documentElement.classList.remove('dark');
     }
   }, [isDarkMode]);
